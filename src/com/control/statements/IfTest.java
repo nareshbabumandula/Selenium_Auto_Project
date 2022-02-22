fe7fa1f77fd55738293494fefb40764efad3ba94
nareshbabumandula <Deepshika123$>
2022-02-22 17:22:28 +0530

    22-Feb-2022: Updated a and b variable values

--- src/com/control/statements/IfTest.java
+++ src/com/control/statements/IfTest.java
@@ -4,8 +4,8 @@ public class IfTest {
 
     public static void main(String[] args) {
 
-        byte a=20;
-        byte b=10;
+        byte a=30;
+        byte b=20;
 
         if(a>b){ // if block
             System.out.println("a is greater than b");
